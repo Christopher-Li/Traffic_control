49c7741c9498438066c906eca8eb5c406daa303a
Alex Wang <alexwang2017@u.northwestern.edu>
2014-10-11 20:43:12 -0500

    refactored single test

---
+++ testAlex/calcSingleXOneWayTime.py
@@ -0,0 +1,14 @@
+def calcSingleXOneWayTotalTime(vehicleRouteFileName,trafficLightFileName):
+	vehicleRouteArray = fileToArray(vehicleRouteFileName)
+	trafficLightArray = fileToArray(trafficLightFileName)
+	totalTime = 0
+	for i in range(0,len(vehicleRouteArray)):
+		j = 0
+		isNS = True
+		while (3 + float(vehicleRouteArray[i][0]) > float(trafficLightArray[j][0])):
+			j += 1
+			isNS = not(isNS)
+		if ((vehicleRouteArray[i][0]=='B' or vehicleRouteArray[i][0]=='D') != (not isNS)):
+			totalTime += float(trafficLightArray[j][0]) - float(vehicleRouteArray[i][0])
+		totalTime += 7
+	return totalTime

7f0dcabe13c55dd9f5e98d0fad71fd691e42fa63
Christopher-Li <Chrisssss158@gmail.com>
2014-10-12 00:24:31 -0500

    I guess I have commits to commit

--- V2.0/classCarCarLinkedList.py
+++ V2.0/classCarCarLinkedList.py
@@ -1,27 +1,37 @@
 class Car:
+	
 	def __init__(self, route, timeCrossSensor):
 		self.route = route
 		self.timeCrossSensor = timeCrossSensor
 		self.next = None
+	
 	def getRoute(self):
 		return self.route
+	
 	def getTimeCrossSensor(self):
 		return self.getTimeCrossSensor
+	
 	def getNext(self):
 		return self.next
+	
 	def setRoute(self,newRoute):
 		self.route = newRoute
+	
 	def setTimeCrossSensor(self, newTimeCrossSensor):
 		self.timeCrossSensor = newTimeCrossSensor
+	
 	def setNext(self,newNext):
 		self.next = newNext
 
 class CarLinkedList:
+	
 	def __init__(self):
 		self.head = None
 		self.tail = None
+	
 	def isEmpty(self):
 		return self.head == None
+	
 	def append(self,item):
 		temp = Car(item)
 		if (self.isEmpty()):
@@ -29,6 +39,7 @@ class CarLinkedList:
 		else:
 			self.tail.next = temp
 			self.tail = temp
+	
 	def size(self):
 		current = self.head
 	    count = 0
@@ -36,16 +47,19 @@ class CarLinkedList:
 	        count = count + 1
 	        current = current.getNext()
 	    return count
+	
 	def removeHead(self):
 		temp = self.head
 		head = temp.next
 		temp.next = None
 		return temp
+	
 	def getElement(self,indexNumber):
 		temp = head
 		for i in range(0,indexNumber):
 			temp = temp.next
 		return temp
+	
 	def getScore(self):
 		score = 0
 		for i in range(0,self.size()):

96cff943059c3db676bd45ee816f9f6d96f43c9c
Alex Wang <alexwang2017@u.northwestern.edu>
2014-10-12 02:51:59 -0500

    new shit

--- testAlex/testFileGenerator.py
+++ testAlex/testFileGenerator.py
@@ -1,5 +1,14 @@
 import random
 
+class routeGenerator:
+	def __init__(self):
+		self.time = 0
+	def generate(self,i):
+		for i in (1,i):
+			time += 
+
+
+
 class intersectionForTestSuite:
 	def __init__(self,value):
 		self.value = value
@@ -13,9 +22,33 @@ class startingPoint:
 		self.next = next
 
 class route:
-	def __init__(self,startingPoint):
+	def __init__(self):
+		i = random.randint(0,3)
+		j = random.randint(0,2)
+		k = random.randint(0,1)
+		if (i == 0):
+			case = 2 if k else 1
+		elif (i == 1):
+			case = 3 if k else 2
+		elif (i == 2):
+			case = 0 if k else 3
+		else:
+			case =1 if k else 0
+		self.routeData = ['A','B','C','D'][i] + str(j)
+		self.nextValue = startingPointArray[i][j].next.value
+		while(self.nextValue != None):
+			self.routeData = self.routeData + str(self.nextValue)
+			l = random.randint(0,1)
+			if case == 0:
+				self.nextValue = intersectionArray[self.nextValue].up if l else intersectionArray[self.nextValue].right
+			elif case == 1:
+				self.nextValue = intersectionArray[self.nextValue].down if l else intersectionArray[self.nextValue].right
+			elif case == 2:
+				self.nextValue = intersectionArray[self.nextValue].down if l else intersectionArray[self.nextValue].left
+			elif case == 3:
+				self.nextValue = intersectionArray[self.nextValue].up if l else intersectionArray[self.nextValue].right								
 
-intersectionArray[None]*9
+intersectionArray = [None]*9
 intersectionArray[0] = intersectionForTestSuite(0)
 intersectionArray[1] = intersectionForTestSuite(1)
 intersectionArray[2] = intersectionForTestSuite(2)
@@ -26,17 +59,17 @@ intersectionArray[6] = intersectionForTestSuite(6)
 intersectionArray[7] = intersectionForTestSuite(7)
 intersectionArray[8] = intersectionForTestSuite(8)
 
-startingPointArray[None]*4
-startingPointArray[0][0] = startingPoint(intersection0)
-startingPointArray[0][1] = startingPoint(intersection1)
-startingPointArray[0][2] = startingPoint(intersection2)
-startingPointArray[1][0] = startingPoint(intersection2)
-startingPointArray[1][1] = startingPoint(intersection5)
-startingPointArray[1][2] = startingPoint(intersection8)
-startingPointArray[2][0] = startingPoint(intersection6)
-startingPointArray[2][1] = startingPoint(intersection7)
-startingPointArray[2][2] = startingPoint(intersection8)
-startingPointArray[3][0] = startingPoint(intersection0)
-startingPointArray[3][1] = startingPoint(intersection3)
-startingPointArray[3][2] = startingPoint(intersection6)
+startingPointArray = [[None,None,None],[None,None,None],[None,None,None],[None,None,None]]
+startingPointArray[0][0] = startingPoint(intersectionArray[0])
+startingPointArray[0][1] = startingPoint(intersectionArray[1])
+startingPointArray[0][2] = startingPoint(intersectionArray[2])
+startingPointArray[1][0] = startingPoint(intersectionArray[2])
+startingPointArray[1][1] = startingPoint(intersectionArray[5])
+startingPointArray[1][2] = startingPoint(intersectionArray[8])
+startingPointArray[2][0] = startingPoint(intersectionArray[6])
+startingPointArray[2][1] = startingPoint(intersectionArray[7])
+startingPointArray[2][2] = startingPoint(intersectionArray[8])
+startingPointArray[3][0] = startingPoint(intersectionArray[0])
+startingPointArray[3][1] = startingPoint(intersectionArray[3])
+startingPointArray[3][2] = startingPoint(intersectionArray[6])
 

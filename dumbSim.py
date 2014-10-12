257f2375bffd1bea921d72b9881e224d233a00e0
Alex Wang <alexwang2017@u.northwestern.edu>
2014-10-12 08:58:33 -0500

    TestSuite

--- dumbSim.py
+++ dumbSim.py
@@ -1,18 +1,15 @@
 class DumbSim:
-    def __init__(self, arrayOfArrivals):
-        self.arrivals = arrayOfArrivals
-    
-    def run(self):
-        returnArray = []
-        
-        lastTimeChange = 30
-        returnArray.append([lastTimeChange, 1])
-        
-        for arrival in self.arrivals:
+    def run(self,vehicleRouteFileName):
+        inputData = fileToArray(vehicleRouteFileName)
+        file_full_name = raw_input("Give output file unique file name: ")
+        f = open(file_full_name,'w')
+        timeInterval = 30
+        lastTimeChange = timeInterval
+        f.write(str(lastTimeChange) + ' 1')
+        for arrival in self.inputData:
             if arrival[0] > (lastTimeChange-3):
-                lastTimeChange += 30
-                returnArray.append([lastTimeChange, 1])
-                
-        lastTimeChange += 30
-        returnArray.append([lastTimeChange, 1])
+                lastTimeChange += timeInterval
+                f.write(str(lastTimeChange) + ' 1')  
+        lastTimeChange += timeInterval
+        f.write(str(lastTimeChange) + ' 1')
         

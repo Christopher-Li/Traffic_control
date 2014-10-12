48f35f4f151d34353b57ac8a217bb009c5ff2645
Christopher-Li <Chrisssss158@gmail.com>
2014-10-11 22:54:18 -0500

    Why am I so bad at commits
    :(

---
+++ V2.0/main.py
@@ -0,0 +1,12 @@
+#import a bunch of crap
+
+file_full_name = raw_input("Where are the car routes!?")
+output_file_full_name = raw_input("Output file?")
+output = open(output_file_full_name,'w')
+
+#vehicleRouteArray = fileToArray(file_full_name)
+time = 0.0
+while (vehicleRouteArray):
+	while vehicleRouteArray[0][0] == time:
+		if sum(vehicleRouteArray[0][1] == ["A","C"])
+		vehicleRouteArray.pop[0]
